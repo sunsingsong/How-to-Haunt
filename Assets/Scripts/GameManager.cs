4f0705ea4a4a5e2083e8cc0e3ac916cb5977d669
Natchanon <natchanon.347@gmail.com>
2020-03-18 22:48:52 +0700

    Add GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+// Game manager singleton
+// Store any game state needed here
+// Find this object in nay script using `FindObjectOfType<GameManager>()`
+
+public class GameManager : MonoBehaviour {
+	public int time = 347;
+}
