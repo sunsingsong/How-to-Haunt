e932a15b5c300cf8bd8e32e782cde8b6cd493adc
sunsingsong <sunspiningsong@gmail.com>
2020-03-19 16:56:00 +0700

    Add humanSight

--- Assets/Scripts/Human_move_1.cs
+++ Assets/Scripts/Human_move_1.cs
@@ -8,6 +8,16 @@ public class Human_move_1 : MonoBehaviour
     public float speed;
 
     private int current;
+    private float angle;
+    private Vector3 direction;
+
+    void Start()
+    {
+        direction = target[current].position - transform.position;
+        angle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg -90;
+        //transform.Rotate(direction * 10.0f, Space.World);
+        transform.eulerAngles = Vector3.up * angle;
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,6 +27,12 @@ public class Human_move_1 : MonoBehaviour
             Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed*Time.deltaTime);
             GetComponent<Rigidbody>().MovePosition(pos);
         }
-        else current = (current+1)%target.Length;
+        else{
+            current = (current+1)%target.Length;
+            direction = target[current].position - target[current-1].position;
+            angle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg -90;
+            transform.eulerAngles = Vector3.up * angle;
+            //transform.Rotate(Vector3.up * 10.0f, Space.World);
+        }
     }
 }
