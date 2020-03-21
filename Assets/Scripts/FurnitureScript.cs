0b3ed4268b4fa2f3679847cc2a3c4f559a6e49be
Natchanon <natchanon.347@gmail.com>
2020-03-21 18:06:19 +0700

    fix furniture

--- Assets/Scripts/FurnitureScript.cs
+++ Assets/Scripts/FurnitureScript.cs
@@ -3,37 +3,30 @@
 public class FurnitureScript : MonoBehaviour {
 	public Color idleColor = new Color(0.98f, 0.48f, 1.0f); // FB7CFF
 	public Color activeColor = new Color(0.47f, 0.1f, 0.61f); // 781A9C
+	public Color cooldownColor = new Color(0f, 0f, 0f);
 
 	public GameObject ghost1;
 	public GameObject ghost2;
 	public GameObject Sight;
 	public GameObject human;
 	private Renderer sight;
-    public GameManager gameManager;
-	private Renderer renderer;
 
 	private bool isCollision1;
 	private bool isCollision2;
 
-	private int cooldown = 0;
-	private bool lastFrameCollide1;
-	private bool lastFrameCollide2;
-	//Checks if ghost is in object last frame or not.
+	private float cooldown = 0;
 
 	void Start(){
 		sight = Sight.GetComponent<Renderer>();
 		isCollision1 = false;
 		isCollision2 = false;
-		lastFrameCollide1 = false;
-		lastFrameCollide2 = false;
 	}
 
 	void Update() {
-		gameManager = FindObjectOfType<GameManager>();
-		renderer = GetComponent<Renderer>();
+		GameManager gameManager = FindObjectOfType<GameManager>();
 		findSawFurniture();
 
-		bool ghost1Intersect = ghost1.GetComponent<Renderer>().bounds.Intersects(renderer.bounds) && cooldown == 0;
+		bool ghost1Intersect = ghost1.GetComponent<Renderer>().bounds.Intersects(GetComponent<Renderer>().bounds);
 		if (ghost1Intersect) {
 			ghost1.GetComponent<Ghost_move1>().ghost1Intersect = ghost1Intersect;
 			isCollision1 = true;
@@ -43,7 +36,7 @@ public class FurnitureScript : MonoBehaviour {
 				isCollision1 = false;
 			}
 		}
-		bool ghost2Intersect = ghost2.GetComponent<Renderer>().bounds.Intersects(renderer.bounds) && cooldown == 0;
+		bool ghost2Intersect = ghost2.GetComponent<Renderer>().bounds.Intersects(GetComponent<Renderer>().bounds);
 		if (ghost2Intersect) {
 			ghost2.GetComponent<Ghost_move2>().ghost2Intersect = ghost2Intersect;
 			isCollision2 = true;
@@ -54,9 +47,12 @@ public class FurnitureScript : MonoBehaviour {
 			}
 		}
 
-		renderer.material.SetColor("_Color", (ghost1Intersect || ghost2Intersect) && !(ghost1Intersect && ghost2Intersect) ? activeColor : idleColor);
+		if (cooldown > 0) {
+			GetComponent<Renderer>().material.SetColor("_Color", cooldownColor);
+		} else {
+			GetComponent<Renderer>().material.SetColor("_Color", (ghost1Intersect || ghost2Intersect) && !(ghost1Intersect && ghost2Intersect) ? activeColor : idleColor);
+		}
 
-		GameManager gameManager = FindObjectOfType<GameManager>();
     	if (ghost1Intersect && ghost2Intersect) {
     		if (gameManager.ghost1ObjectName == name) gameManager.ghost1ObjectName = "";
     		if (gameManager.ghost2ObjectName == name) gameManager.ghost2ObjectName = "";
@@ -73,32 +69,22 @@ public class FurnitureScript : MonoBehaviour {
 			}
     	}
 
-		if (ghost1Intersect) {
-			lastFrameCollide1 = true;
-		} else if (lastFrameCollide1) {
-			lastFrameCollide1 = false;
-			cooldown = 1000;
-		}
-
-		if (ghost2Intersect) {
-			lastFrameCollide2 = true;
-		} else if (lastFrameCollide2) {
-			lastFrameCollide2 = false;
-			cooldown = 1000;
-		}
-		
 		if (cooldown > 0) {
 			cooldown -= Time.deltaTime;
 		}
 	}
 
-	bool isActivatable() {
-		return cooldown <= 0;
+	public void activate() {
+		if (cooldown > 0) return;
+		cooldown = 1000;
+
 	}
 
-	private void findSawFurniture(){
+	private void findSawFurniture() {
+		GameManager gameManager = FindObjectOfType<GameManager>();
+
 		if(sight != null){
-			if(renderer.bounds.Intersects(sight.bounds)){
+			if(GetComponent<Renderer>().bounds.Intersects(sight.bounds)){
 				 gameManager.sawFurnitures[name] = true;
 				 Debug.Log(name+" TRUE");
 			}
