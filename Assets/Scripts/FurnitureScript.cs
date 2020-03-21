4c76a74611c593240fe71f07a2f0039f4cb5d38d
Natchanon <natchanon.347@gmail.com>
2020-03-21 16:27:06 +0700

    furniture cooldown

--- Assets/Scripts/FurnitureScript.cs
+++ Assets/Scripts/FurnitureScript.cs
@@ -11,41 +11,52 @@ public class FurnitureScript : MonoBehaviour {
 	private Renderer sight;
     public GameManager gameManager;
 	private Renderer renderer;
+
 	private bool isCollision1;
 	private bool isCollision2;
+
+	private int cooldown = 0;
+	private bool lastFrameCollide1;
+	private bool lastFrameCollide2;
+	//Checks if ghost is in object last frame or not.
 
 	void Start(){
 		sight = Sight.GetComponent<Renderer>();
 		isCollision1 = false;
 		isCollision2 = false;
+		lastFrameCollide1 = false;
+		lastFrameCollide2 = false;
 	}
 
 	void Update() {
 		gameManager = FindObjectOfType<GameManager>();
 		renderer = GetComponent<Renderer>();
 		findSawFurniture();
-		bool ghost1Intersect = ghost1.GetComponent<Renderer>().bounds.Intersects(renderer.bounds);
-		if (ghost1Intersect){
+
+		bool ghost1Intersect = ghost1.GetComponent<Renderer>().bounds.Intersects(renderer.bounds) && cooldown == 0;
+		if (ghost1Intersect) {
 			ghost1.GetComponent<Ghost_move1>().ghost1Intersect = ghost1Intersect;
-			isCollision1=true;
-		}else{
-			if (isCollision1 && !ghost1Intersect){
+			isCollision1 = true;
+		} else {
+			if (isCollision1 && !ghost1Intersect) {
 				ghost1.GetComponent<Ghost_move1>().ghost1Intersect = false;
-				isCollision1=false;
+				isCollision1 = false;
 			}
 		}
-		bool ghost2Intersect = ghost2.GetComponent<Renderer>().bounds.Intersects(renderer.bounds);
-		if (ghost2Intersect){
+		bool ghost2Intersect = ghost2.GetComponent<Renderer>().bounds.Intersects(renderer.bounds) && cooldown == 0;
+		if (ghost2Intersect) {
 			ghost2.GetComponent<Ghost_move2>().ghost2Intersect = ghost2Intersect;
-			isCollision2=true;
-		}else{
-			if (isCollision2){
+			isCollision2 = true;
+		} else {
+			if (isCollision2) {
 				ghost2.GetComponent<Ghost_move2>().ghost2Intersect = false;
-				isCollision2=false;
+				isCollision2 = false;
 			}
 		}
 
 		renderer.material.SetColor("_Color", (ghost1Intersect || ghost2Intersect) && !(ghost1Intersect && ghost2Intersect) ? activeColor : idleColor);
+
+		GameManager gameManager = FindObjectOfType<GameManager>();
     	if (ghost1Intersect && ghost2Intersect) {
     		if (gameManager.ghost1ObjectName == name) gameManager.ghost1ObjectName = "";
     		if (gameManager.ghost2ObjectName == name) gameManager.ghost2ObjectName = "";
@@ -61,6 +72,28 @@ public class FurnitureScript : MonoBehaviour {
 				gameManager.ghost2ObjectName = "";
 			}
     	}
+
+		if (ghost1Intersect) {
+			lastFrameCollide1 = true;
+		} else if (lastFrameCollide1) {
+			lastFrameCollide1 = false;
+			cooldown = 1000;
+		}
+
+		if (ghost2Intersect) {
+			lastFrameCollide2 = true;
+		} else if (lastFrameCollide2) {
+			lastFrameCollide2 = false;
+			cooldown = 1000;
+		}
+		
+		if (cooldown > 0) {
+			cooldown -= Time.deltaTime;
+		}
+	}
+
+	bool isActivatable() {
+		return cooldown <= 0;
 	}
 
 	private void findSawFurniture(){
